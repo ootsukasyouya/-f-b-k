f50943fe0b165e686d5ec0339b492f30e60d74ac
ootsukasyouya <rikkuhose@gmail.com>
2021-10-28 19:19:28 +0900

    ログイン機能作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_27_131441) do
+ActiveRecord::Schema.define(version: 2021_10_27_135527) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
